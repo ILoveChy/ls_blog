dc051bb002341b256a6a2b4bb9a17e3547dc374d
liangshuai <526995681@qq.com>
2019-09-17 23:38:10 +0800

    1

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ var globalConfig = {};
 
 var conf = fs.readFileSync('./server.conf');
 
-var configArr = conf.toString().split('\r\n');
+var configArr = conf.toString().split('\n');
 
 for (var i = 0; i < configArr.length; i++) {
-    globalConfig[configArr[i].split('=')[0]] = configArr[i].split('=')[1].trim();
+    globalConfig[configArr[i].split('=')[0].trim()] = configArr[i].split('=')[1].trim();
 }
 module.exports = globalConfig;
