c6c956ff87e3edc5ca746ddf8880c46904b68520
婉月歌 <37863087+ILoveChy@users.noreply.github.com>
2019-09-20 16:26:25 +0800

    Update dbutil.js

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -5,7 +5,7 @@ function createConnection() {
         host: "192.168.31.92",
         port: "3306",
         user: "root",
-        password: "liangshuai",
+        password: "Ls_526995687",
         database: "ls_bolg"
     });
     return connection;
